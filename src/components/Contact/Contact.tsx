ef1d3b74329f2c197570cae50729e7ca6f799707
Leticia <leticia.fraga@mindsdigital.net>
2023-09-05 23:45:38 -0300

    fix links and update texts

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,9 @@
-import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { faCircle, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ReactElement } from "react";
-import style from "./Contact.module.scss";
-import { useTranslation } from "react-i18next";
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faCircle, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ReactElement } from 'react';
+import style from './Contact.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface SocialLink {
     icon: ReactElement;
@@ -14,7 +14,7 @@ interface SocialLink {
 const socials: SocialLink[] = [
     {
         icon: <FontAwesomeIcon icon={faGithub} />,
-        link: "https://github.com/leticiafraga/",
+        link: 'https://github.com/leticiafraga/',
     },
     {
         icon: (
@@ -24,7 +24,7 @@ const socials: SocialLink[] = [
                 transform="shrink-5"
             />
         ),
-        link: "https://www.linkedin.com/in/leticia-fraga/",
+        link: 'https://www.linkedin.com/in/leticia-fraga/',
         bg: <FontAwesomeIcon icon={faCircle} />,
     },
     {
@@ -35,27 +35,27 @@ const socials: SocialLink[] = [
                 transform="shrink-5"
             />
         ),
-        link: "mailto:leticiafraga55@gmail.com",
+        link: 'mailto:leticiafraga55@gmail.com',
         bg: <FontAwesomeIcon icon={faCircle} />,
     },
 ];
 
 export default function Contact() {
-    const { t } = useTranslation("app");
+    const { t } = useTranslation('app');
 
     return (
         <section
             className={
-                style.contact + " row align-items-center justify-content-center"
+                style.contact + ' row align-items-center justify-content-center'
             }
             id="contact"
         >
             <div className="col-12 col-md d-flex justify-content-center">
-                <h2>{t("contact")}</h2>
+                <h2>{t('contact')}</h2>
             </div>
             <div className="col-12 col-md-7">
                 {socials.map((item) => (
-                    <a href={item.link} key={item.link}>
+                    <a href={item.link} key={item.link} target="_blank">
                         <span className="ms-4 fa-layers fa-4x">
                             {item.bg}
                             {item.icon}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,10 @@
-import style from "./Projects.module.scss";
-import pilates from "../../assets/studio-pilates.webp";
-import portalFilmes from "../../assets/portal-filmes.webp";
-import sadfes from "../../assets/sadfes.webp";
-import portfolio from "../../assets/portfolio.webp";
-import { useTranslation } from "react-i18next";
-import type appType from "../../public/locales/pt/app.json";
+import style from './Projects.module.scss';
+import pilates from '../../assets/studio-pilates.webp';
+import portalFilmes from '../../assets/portal-filmes.webp';
+import sadfes from '../../assets/sadfes.webp';
+import portfolio from '../../assets/portfolio.webp';
+import { useTranslation } from 'react-i18next';
+import type appType from '../../public/locales/pt/app.json';
 
 interface Project {
     link: string;
@@ -14,32 +14,32 @@ interface Project {
 }
 
 export default function Projects() {
-    const { t } = useTranslation("app");
+    const { t } = useTranslation('app');
 
     const projects: Project[] = [
         {
-            key: "athletesMonitoring",
-            link: "https://tis4-demo.web.app/",
+            key: 'athletesMonitoring',
+            link: 'https://tis4-demo.web.app/',
             image: sadfes,
-            github: "https://github.com/leticiafraga/tis4-demo",
+            github: 'https://github.com/leticiafraga/tis4-demo',
         },
         {
-            key: "moviesPortal",
-            link: "https://leticiafraga.github.io/portal-filmes/index.html",
+            key: 'moviesPortal',
+            link: 'https://leticiafraga.github.io/portal-filmes/index.html',
             image: portalFilmes,
-            github: "https://github.com/leticiafraga/portal-filmes",
+            github: 'https://github.com/leticiafraga/portal-filmes',
         },
         {
-            key: "portfolio",
-            link: "https://leticiafraga.github.io/",
+            key: 'portfolio',
+            link: 'https://leticiafraga.github.io/',
             image: portfolio,
-            github: "https://github.com/leticiafraga/leticiafraga.github.io",
+            github: 'https://github.com/leticiafraga/leticiafraga.github.io',
         },
         {
-            key: "pilates",
-            link: "https://studiozenpilates.web.app/",
+            key: 'pilates',
+            link: 'https://studiozenpilates.web.app/',
             image: pilates,
-            github: "https://github.com/leticiafraga/studio-pilates",
+            github: 'https://github.com/leticiafraga/studio-pilates',
         },
     ];
 
@@ -47,14 +47,14 @@ export default function Projects() {
         <section
             className={
                 style.projects +
-                " row align-items-center justify-content-center px-5"
+                ' row align-items-center justify-content-center px-5'
             }
             id="projects"
         >
             <div className="col-12 col-md mb-5 mb-lg-0 d-flex justify-content-center">
-                <h2>{t("projects")}</h2>
+                <h2>{t('projects')}</h2>
             </div>
-            <div className={"col-12 col-lg-7 " + style.wrapper}>
+            <div className={'col-12 col-lg-7 ' + style.wrapper}>
                 {projects.map((p) => (
                     <div className={style.flipCard} key={p.key}>
                         <div className={style.flipCardInner}>
@@ -79,18 +79,18 @@ export default function Projects() {
                                 <p>{t(`projectsSection.${p.key}.desc`)}</p>
                                 <div className="row w-100">
                                     <div className="col-6">
-                                        <button className="w-100 btn btn-secondary m-2">
-                                            <a target="_blank" href={p.link}>
-                                                {t("access")}
-                                            </a>
-                                        </button>
+                                        <a target="_blank" href={p.link}>
+                                            <button className="w-100 btn btn-secondary m-2">
+                                                {t('access')}
+                                            </button>
+                                        </a>
                                     </div>
                                     <div className="col-6">
-                                        <button className="w-100 btn btn-secondary m-2">
-                                            <a target="_blank" href={p.github}>
+                                        <a target="_blank" href={p.github}>
+                                            <button className="w-100 btn btn-secondary m-2">
                                                 GitHub
-                                            </a>
-                                        </button>
+                                            </button>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
